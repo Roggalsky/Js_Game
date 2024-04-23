1239f224ee65d950b26d0d166e5e4cac0e0b53c9
Roggalsky <140446164+Roggalsky@users.noreply.github.com>
2024-04-23 11:13:15 +0200

    drawLangFunctionAdd

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-const drawLanguage = function () {};
-
-const html = `HTML`;
-const js = `JAVASCRIPT`;
-const react = `REACT`;
-const gitHub = `GITHUB`;
-const node = `NODE.JS`;
+const langArray = [`GITHUB`, `NODE.JS`, `REACT`, `JAVASCRIPT`, `HTML`];
+const drawLanguage = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+  }
+};
+const chosenLnag = drawLanguage(langArray);
 
 const shuffleString = function (string) {
   const stringToArray = string.split("");
-
   //   Fisher-Yates algorithm
   for (let i = stringToArray.length - 1; i > 0; i--) {
     const random = Math.floor(Math.random() * (i + 1));
@@ -21,8 +21,29 @@ const shuffleString = function (string) {
   return String(stringToArray).replaceAll(",", " ");
 };
 
-document.getElementById("mixedLetters").textContent = shuffleString(js);
+document.getElementById("mixedLetters").textContent = shuffleString(chosenLnag);
 
+const deleteBTN = document.querySelector(".backspace");
+let chars = [];
+const buttons = document.querySelectorAll(".btn");
+const textPlace = document.querySelector(".typeYourAnswer");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    textPlace.value += btn.innerText;
+    chars = textPlace.value.split("");
+  });
+});
+deleteBTN.addEventListener("click", () => {
+  chars.pop();
+  textPlace.value = chars.join("");
+});
+
+const checkButton = document.querySelector(".confirm");
+checkButton.addEventListener("click", () => {
+  if (textPlace.value === chosenLnag) {
+    console.log(`działam`);
+  }
+});
 //  Fisher-Yates algorithm
 // function shuffleArray(array) {
 //     for (let i = array.length - 1; i > 0; i--) {
@@ -35,18 +56,3 @@ document.getElementById("mixedLetters").textContent = shuffleString(js);
 
 //   na początku chciałem uzyć
 //   const futureIndex = Math.floor(Math.random() * stringToArray.length) + 1;
-const deleteBTN = document.querySelector(".backspace");
-let chars = [];
-const buttons = document.querySelectorAll(".btn");
-const textPlace = document.querySelector(".typeYourAnswer");
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    textPlace.value += btn.innerText;
-    chars = textPlace.value.split("");
-    console.log(chars);
-  });
-});
-deleteBTN.addEventListener("click", () => {
-  chars.pop();
-  textPlace.value = chars.join("");
-});
